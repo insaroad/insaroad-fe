6e645485324754113a0da66ae1627d19537b5353
nahjjun <nahjjun1125@naver.com>
2025-12-05 22:51:56 +0900

    :lipstick: style:index.css의 #root height 100% -> 100vh로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StartPage from './pages/kiosk/startPage/StartPage';
 import EnterNumPage from './pages/kiosk/enterNumPage/EnterNumPage';
+import MissionDescriptionPageAnimal from './pages/kiosk/missionDescriptionPageAnimal/MissionDescriptionPageAnimal';
 import './App.css';
 
 export function App() {
@@ -10,6 +11,9 @@ export function App() {
                 <Route path="/kiosk">
                     <Route index element={<StartPage />} />
                     <Route path="keep" element={<EnterNumPage />} />
+                    <Route path="mission">
+                        <Route path="animal" element={<MissionDescriptionPageAnimal />} />
+                    </Route>
                 </Route>
             </Routes>
         </BrowserRouter>
