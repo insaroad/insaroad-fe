fe31d037ff52c1428e1464337b1ce945950b6995
jaeyeon <kasto6329@gmail.com>
2025-12-19 22:07:59 +0900

    ♻️  Refactor env 변수 명 수정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
 
-const BASE_URL = import.meta.env.VITE_SERVER_BASE_URL;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const defaultInstance: AxiosInstance = axios.create({
   baseURL: BASE_URL,
